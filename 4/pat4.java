9293d1484fc848cc7a8e2e4cd9dc41e48279d077
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2026-07-16 12:35:04 +0530

    Create pat4.java

---
+++ 4/pat4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void pattern4(int n) {
+        for (int i = 1; i <= n; i++){
+            for ( int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int n = 5;
+        sol.pattern4(n);
+    }
+}
