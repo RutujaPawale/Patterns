db9bd0cc152c9e937a98b51837ec5f321a4e1ad6
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2026-07-16 12:30:45 +0530

    Create pat3.java

---
+++ 3/pat3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void pattern3(int n) {
+        for (int i = 1; i < n+1; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int n = 5;
+        sol.pattern3(n);
+    }
+}
