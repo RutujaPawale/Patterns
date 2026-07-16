fb35bd15c0bcf167d121d6c77be42ca79033dbe7
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2026-07-16 17:39:58 +0530

    Create pat11.java

---
+++ 11/pat11.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void pattern11(int n) {
+	int start = 1;
+        for (int i = 0; i < n; i++){
+            if (i % 2 == 0){
+                start = 1;
+            } else {
+                start = 0;
+            }
+            for (int j = 0; j <= i; j++){
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int n = 5;
+        sol.pattern11(n);
+    }
+}
