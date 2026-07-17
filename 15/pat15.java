293fce1620f90b284d3fa2a02edbb757a278109e
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2026-07-17 11:01:13 +0530

    Create pat15.java

---
+++ 15/pat15.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void pattern15(int n) {
+        for (int i = 0; i < n; i++){
+            for (char ch = 'A'; ch <= 'A' + (n- i - 1); ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int n = 5;
+        sol.pattern15(n);
+    }
+}
