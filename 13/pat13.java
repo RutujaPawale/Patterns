8300406739d5a97444cbb75c1debc8758885b8c6
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2026-07-17 10:41:37 +0530

    Create pat13.java

---
+++ 13/pat13.java
@@ -0,0 +1,17 @@
+ class Solution {
+    public void pattern13(int n) {
+        int num = 1;
+        for (int i = 1; i <=n; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(num + " ");
+                num += 1;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int n = 5;
+        sol.pattern13(n);
+    }
+}
